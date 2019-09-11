6f94b42fdcd9fe1c0d9be2f5301842a5ccc11ff2
santiagoiv <santiagoivan46@gmail.com>
2019-09-10 20:14:48 -0600

    se levanto el server

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/*
-    información básica de las películas como, nombre, género, actores y descripción. Tu eres el encargado de diseñar la base de datos para esta app.
-*/
+
 
 require('dotenv').config();
 require('./database');
